5d255c287c4c137d6b4107cbc3124d8326f45ed6
Damian-luis <damian.luis.porta@gmail.com>
2022-06-22 17:40:05 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -67,8 +67,8 @@ function App() {
       </div>
 
       <div className="laboral-experience">
-        <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. </h2>
-        <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever</h3>
+        <h3>En cuanto a mi experiecia laboral trabajé como desarrolador backend, creando apis para el consumo del frontend, cual, dicho sea de paso, me desempeñé en ambos cargos.</h3>
+        <h3>Por supusto no todas son habilidades tecnicas, en ese trabajo desarrollé las siguientes habilidades tales como.</h3>
         <ul>
           <li>Trabajar en equipo</li>
           <li>Metodologias agiles</li>
